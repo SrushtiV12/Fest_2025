ab38aa905c211a6ac9188998c84b2c4e1c883ac2
SrushtiV12 <srushti20pute1@gamil.com>
2025-10-27 16:04:42 +0530

    Added Selection Sort implementation in Java

---
+++ Sorting/Selection sort.java
@@ -0,0 +1,47 @@
+// Selection Sort in Java
+public class SelectionSort {
+
+    // Function to perform selection sort
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+
+        // Traverse through all array elements
+        for (int i = 0; i < n - 1; i++) {
+            // Assume the current index has the smallest value
+            int minIndex = i;
+
+            // Find the smallest element in the remaining unsorted part
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            // Swap the found minimum element with the first element
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    // Function to print the array
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    // Main method to test the algorithm
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+
+        System.out.println("Original array:");
+        printArray(arr);
+
+        selectionSort(arr);
+
+        System.out.println("Sorted array:");
+        printArray(arr);
+    }
+}
